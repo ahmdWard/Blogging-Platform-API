01ab431a93ff3bf6d7103409ab0392228096a662
ahmdWard <ahmdemadabdallah@gmail.com>
2024-08-31 02:32:50 +0300

    adding controllers sections

---
+++ controllers/blogController.js
@@ -0,0 +1,28 @@
+const AppError=require('../utils/appError')
+
+
+exports.getBlog= (req,res,next)=>{
+ 
+
+}
+
+exports.createBlog= (req,res,next)=>{
+ 
+
+}
+
+
+exports.getAllBlogs= (req,res,next)=>{
+ 
+
+}
+
+exports.updateBlog= (req,res,next)=>{
+ 
+
+}
+
+exports.deleteBlog= (req,res,next)=>{
+ 
+
+}
