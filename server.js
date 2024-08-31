9cc4951504485aaaf86077ea9faa6cb619bcf13f
ahmdWard <ahmdemadabdallah@gmail.com>
2024-08-31 04:27:55 +0300

    mongo model

--- server.js
+++ server.js
@@ -2,26 +2,8 @@ const dotenv= require('dotenv')
 
 dotenv.config(); 
 
-const  postgres = require('pg');
-
 const app= require('./app')
 
-const client = new postgres.Client({
-	user:process.env.DATABASE_USER,
-	password:process.env.DATABASE_PASSWORD,
-	host: process.env.DATABASE_host,
-	port: process.env.DATABASE_PORT,
-	database: process.env.DATABASE_NAME,
-});
-
-client
-	.connect()
-	.then(() => {
-		console.log('Connected to PostgreSQL database');
-	})
-	.catch((err) => {
-		console.error('Error connecting to PostgreSQL database', err);
-	});
 
 const port= process.env.PORT || 8000
 
