21f732d012d6396124fbf93c01f1408f9d86ecbf
ahmdWard <ahmdemadabdallah@gmail.com>
2024-09-01 02:11:57 +0300

    adding updating and deleting

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,10 +4,10 @@ const AppError=require('../utils/appError')
 
 
 exports.getBlog= async (req,res,next)=>{
-    try {
+
         const result = await client.query(`select * from blogs where id = ${req.params.id}`)
         const blog =result.rows
-        if(!blog[0])
+        if(!blog.length)
            return next(new AppError('no blog with this ID'),404)
        
         res.status(200).json({
@@ -16,9 +16,7 @@ exports.getBlog= async (req,res,next)=>{
                 blog
             }
         })
-    } catch (error) {
-        next(new AppError('there is a problem in getting back blog'),400)
-    }
+    
 
 }
 
@@ -67,12 +65,58 @@ exports.getAllBlogs= async (req,res,next)=>{
 
 }
 
-exports.updateBlog= (req,res,next)=>{
+exports.updateBlog= async (req,res,next)=>{
+
+      const updates = req.body
+      console.log(updates)
+
+      const keys= Object.keys(updates)
+      const values= Object.values(updates)
+      console.log(keys);
+      console.log(values);
+      const setClause= Object.keys(updates).map((key,index)=>`${key}=$${index+1}`).join(',')
+      console.log(setClause)
+      if(!keys.length)
+        return next(new AppError('No fields to update', 400))
+
+  try{
+    const result= await client.query(`update  blogs set ${setClause} where id = ${req.params.id} RETURNING *`,
+        values
+      )
+      console.log(result)   
+      
+
+
+      res.status(200).json({
+        status:"success",
+        data:{
+            result
+        }
+      })
+    
+  }
+    catch (err) {
+        console.error('Error updating blog:', err);
+        return next(new AppError('There was a problem updating the blog', 500));
+      }
+  }
+   
+
+
+
+exports.deleteBlog=async (req,res,next)=>{
+
+        const result = await client.query(
+            'delete from blogs where id = $1 RETURNING *',[req.params.id]
+        )
+        if(!result.rows.length)
+           return next(new AppError('this id is not found'),400)
+      res.status(204).json({
+        status:"sucess",
+        data:{
+            result
+        }
+      })
  
-
-}
-
-exports.deleteBlog= (req,res,next)=>{
  
-
 }
